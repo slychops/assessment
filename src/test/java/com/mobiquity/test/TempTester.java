e8bd82bd11ed5a0b91d82b141d68a25cb9ea7f08
Richard <richies75@gmail.com>
2019-09-26 20:16:22 +0200

    latest update

---
+++ src/test/java/com/mobiquity/test/TempTester.java
@@ -0,0 +1,14 @@
+package com.mobiquity.test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TempTester {
+
+    @Test
+    public void firstTestDoNothing() {
+        assertEquals(0, 0);
+    }
+
+}
